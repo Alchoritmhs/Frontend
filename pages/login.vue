152afad9aeb6bfd46f384c7aba8df140b38201df
Amir <amir@musaev.online>
2020-10-17 14:00:39 +0000

    refactor: Login page

--- pages/login.vue
+++ pages/login.vue
@@ -2,24 +2,17 @@
   <v-card
     class="d-flex flex-column mx-auto"
     width="400"
-    height="300"
     style="margin-top: 20vh"
   >
     <v-card-text class="text-center" style="font-size: 20px">
       Вход
     </v-card-text>
     <v-form class="d-flex flex-column align-center px-6">
-      <v-text-field
-        v-model="firstname"
-        label="Логин"
-        style="width: 100%"
-      />
-      <v-text-field
-        v-model="firstname"
-        label="Пароль"
-        style="width: 100%"
-      />
-      <v-btn @click="$router.replace('/')" class="mt-3" width="120">Войти</v-btn>
+      <v-text-field label="Логин" style="width: 100%" />
+      <v-text-field label="Пароль" style="width: 100%" />
+      <v-btn @click="$router.replace('/')" class="mt-3 mb-6" width="120"
+        >Войти</v-btn
+      >
     </v-form>
   </v-card>
 </template>
