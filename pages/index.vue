7e08095dcec79a360acf50b9f1b7fd46c27ade90
Amir <amir@musaev.online>
2020-10-17 15:29:55 +0000

    refactor: App layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,22 @@
 <template>
   <div>
     <header
-      style="height: 56px; box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.6)"
+      style="height: 56px; box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.4)"
     ></header>
     <main class="d-flex">
       <aside
         class="d-flex flex-column pl-3 pr-5"
         style="
-          width: 350px;
+          width: 370px;
           height: 100vh;
-          box-shadow: 10px 0 10px -10px rgba(0, 0, 0, 0.4);
+          font-size: 15.5px;
+          box-shadow: 10px 0 10px -10px rgba(0, 0, 0, 0.2);
         "
       >
-        <div class="py-2" style="box-shadow: 0 3px 2px -2px rgba(0, 0, 0, 0.2)">
+        <div class="py-2" style="color: rgba(0, 0, 0, 0.6)">
           Документы на подпись
         </div>
-        <div class="py-2" style="box-shadow: 0 3px 2px -2px rgba(0, 0, 200, 0.9)">
+        <div class="py-2 indigo--text text--lighten-1" style="">
           Подписанные документы
         </div>
       </aside>
@@ -24,15 +25,12 @@
         style="
           width: 200px;
           height: 100vh;
-          box-shadow: 10px 0px 10px -10px rgba(0, 0, 0, 0.4);
+          font-size: 14px;
+          box-shadow: 10px 0px 10px -10px rgba(0, 0, 0, 0.2);
         "
       >
-        <div class="py-2" style="box-shadow: 0 3px 2px -2px rgba(0, 0, 200, 0.9)">
-          Документ 1
-        </div>
-        <div class="py-2" style="box-shadow: 0 3px 2px -2px rgba(0, 0, 0, 0.2)">
-          Документ 2
-        </div>
+        <div class="py-2 indigo--text text--lighten-1" style="">Документ 1</div>
+        <div class="py-2" style="color: rgba(0, 0, 0, 0.6)">Документ 2</div>
       </aside>
       <v-container> Документ 1... </v-container>
     </main>
