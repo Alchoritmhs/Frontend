1fee4786fb91f0318cba558737f59cb254f0a8d2
Amir <amir@musaev.online>
2020-10-17 23:49:31 +0000

    feat: Document template form

---
+++ components/template-form.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-dialog v-model="dialog" persistent max-width="600px">
+    <template v-slot:activator="{ on, attrs }">
+      <span v-bind="attrs" v-on="on"> Создать документ по шаблону </span>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="headline">Поля шаблона</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-text-field label="Поле" />
+          <v-text-field label="Поле" />
+          <v-text-field label="Поле" />
+          <v-text-field label="Поле" />
+          <v-text-field label="Поле" />
+          <v-text-field label="Поле" />
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click="dialog = false">
+          Закрыть
+        </v-btn>
+        <v-btn color="blue darken-1" text @click="dialog = false">
+          Создать
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+    }
+  },
+}
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,36 @@
 <template>
   <div>
     <header
+      class="d-flex align-center"
       style="height: 56px; box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.4)"
-    ></header>
+    >
+      <v-avatar
+        v-if="user"
+        @click="user = !user"
+        size="36"
+        color="red"
+        class="ml-auto mr-16"
+        style="cursor: pointer"
+      >
+        <span class="white--text">CJ</span>
+      </v-avatar>
+      <v-avatar
+        v-else
+        @click="user = !user"
+        size="36"
+        color="indigo"
+        class="ml-auto mr-16"
+        style="cursor: pointer"
+      >
+        <span class="white--text">Am</span>
+      </v-avatar>
+    </header>
     <main class="d-flex">
       <aside
         class="d-flex flex-column"
         style="
           width: 400px;
-          height: 100vh;
+          height: calc(100vh - 56px);
           font-size: 15px;
           border-right: 1px solid rgba(0, 0, 0, 0.05);
         "
@@ -17,6 +39,7 @@
           <v-list-item-group v-model="selectedMenuOption" color="primary">
             <v-list-item :key="0">Документы на подпись</v-list-item>
             <v-list-item :key="1">Подписанные документы</v-list-item>
+            <v-list-item :key="2"><template-form /></v-list-item>
           </v-list-item-group>
         </v-list>
       </aside>
@@ -24,7 +47,7 @@
         class="d-flex flex-column ml-1"
         style="
           width: 220px;
-          height: 100vh;
+          height: calc(100vh - 56px);
           font-size: 14px;
           box-shadow: 10px 0px 10px -10px rgba(0, 0, 0, 0.1);
         "
@@ -45,7 +68,7 @@
         <iframe
           src="https://view.officeapps.live.com/op/embed.aspx?src=http://www.learningaboutelectronics.com/Articles/NP-modernization-act-new-york-state.doc"
           width="80%"
-          height="565px"
+          height="90%"
           frameborder="0"
         >
         </iframe>
@@ -55,10 +78,15 @@
 </template>
 
 <script>
+import TemplateForm from '../components/template-form'
 export default {
+  components: {
+    TemplateForm,
+  },
   data() {
     return {
       selectedMenuOption: 0,
+      user: true,
     }
   },
 }
