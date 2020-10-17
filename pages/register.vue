f6e0afef490a25cd34f982e5a4963e2a5e2cd527
Amir <amir@musaev.online>
2020-10-17 13:59:45 +0000

    feat: Add register page

---
+++ pages/register.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card
+    class="d-flex flex-column mx-auto"
+    width="400"
+    style="margin-top: 20vh"
+  >
+    <v-card-text class="text-center" style="font-size: 20px">
+      Регистрация
+    </v-card-text>
+    <v-form class="d-flex flex-column align-center px-6">
+      <v-text-field label="Имя" style="width: 100%" />
+      <v-text-field label="Фамилия" style="width: 100%" />
+      <v-text-field label="Отчество" style="width: 100%" />
+      <v-text-field label="Почта" style="width: 100%" />
+      <v-btn @click="$router.replace('/')" class="mt-3 my-6" width="120"
+        >Вперед!</v-btn
+      >
+    </v-form>
+  </v-card>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
